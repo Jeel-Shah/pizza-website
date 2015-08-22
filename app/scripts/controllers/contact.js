413a3b815c1fa33ddde9c9db29e5425696ebeb2b
Jeel Shah <jeelshah96@gmail.com>
2015-08-21 23:14:40 -0400

    attempt to add google maps

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -1,4 +1,12 @@
 'use strict';
 
 angular.module('pizzaWebsiteApp')
-  .controller('ContactCtrl', function($scope){});
+  .controller('ContactCtrl', function($scope){
+    $scope.map = {
+      center: {
+        latitude: 45,
+        longitude: -73
+      },
+      zoom: 8
+    };
+});
